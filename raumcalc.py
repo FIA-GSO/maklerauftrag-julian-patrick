e96e1f2c6cce8f8cdd258a8427e34263935717e6
xdepRaven <buhr.patrick97@gmail.com>
2024-02-15 08:30:06 +0100

    refactoring done

--- raumcalc.py
+++ raumcalc.py
@@ -7,23 +7,10 @@ def run():
     while weiterer_raum:
         teilrechteck = True
         gesamt_raum = 0
-
         print("Geben sie den Namen des Raumes an: ")
         name_raum = input()
 
-        while teilrechteck:
-            print("Geben sie die Breite ihres Rechteckes/ Teilrechteckes an: ")
-            breite = float(input())
-            print("Geben sie nun die Länge an: ")
-            laenge = float(input())
-
-            gesamt_raum = gesamt_raum + (breite * laenge)
-
-            print("Noch ein weiteres Teilrechteck?(Y/N)")
-            userinput = input().lower()
-            if userinput == "n":
-                teilrechteck = False
-
+        gesamt_raum = calc_teilrechteck(gesamt_raum, teilrechteck)
         raum_list.update({name_raum: gesamt_raum})
         gesamt_wohnung = gesamt_wohnung + gesamt_raum
         anzahl_raum = anzahl_raum + 1
@@ -34,9 +21,28 @@ def run():
             weiterer_raum = False
 
     raum_durchschnitt = gesamt_wohnung / anzahl_raum
+    print_list(gesamt_wohnung, raum_durchschnitt, raum_list)
 
+
+def calc_teilrechteck(gesamt_raum, teilrechteck):
+    while teilrechteck:
+        print("Geben sie die Breite ihres Rechteckes/ Teilrechteckes an: ")
+        breite = float(input())
+        print("Geben sie nun die Länge an: ")
+        laenge = float(input())
+
+        gesamt_raum = gesamt_raum + (breite * laenge)
+
+        print("Noch ein weiteres Teilrechteck?(Y/N)")
+        userinput = input().lower()
+        if userinput == "n":
+            teilrechteck = False
+    return gesamt_raum
+
+
+def print_list(gesamt_wohnung, raum_durchschnitt, raum_list):
     for raum in raum_list:
-        print(f"{raum}: {raum_list[raum]}")
+        print(f"{raum}: {raum_list[raum]}m²")
     print(f"Gesamtgröße: {gesamt_wohnung}m²")
     print(f"Durschnittsraumgröße: {raum_durchschnitt}m²")
 

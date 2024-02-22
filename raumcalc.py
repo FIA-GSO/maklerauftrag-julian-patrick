9727e3a7a0c605f5bc3368eea4bcebff221f3ab1
A1D5A7D <Julian.Grzanna@rewe-group.com>
2024-02-22 09:08:30 +0100

    Added input validation

--- raumcalc.py
+++ raumcalc.py
@@ -1,3 +1,6 @@
+import re
+
+
 def run():
     raum_list = {}
     weiterer_raum = True
@@ -9,7 +12,7 @@ def run():
         teilrechteck = True
         gesamt_raum = 0
         print("Geben sie den Namen des Raumes an: ")
-        name_raum = input()
+        name_raum = name_input()
 
         gesamt_raum = calc_teilrechteck(gesamt_raum, teilrechteck)
         raum_list.update({name_raum: gesamt_raum})
@@ -18,7 +21,7 @@ def run():
 
         # Abfrage ob weiterer Raum vorhanden
         print("Einen weiteren Raum? (Y/N) ")
-        userinput = input().lower()
+        userinput = yes_no_input()
         if userinput == "n":
             weiterer_raum = False
 
@@ -30,14 +33,14 @@ def run():
 def calc_teilrechteck(gesamt_raum, teilrechteck):
     while teilrechteck:
         print("Geben sie die Breite ihres Rechteckes/ Teilrechteckes an: ")
-        breite = float(input())
+        breite = float(number_input())
         print("Geben sie nun die Länge an: ")
-        laenge = float(input())
+        laenge = float(number_input())
 
         gesamt_raum = gesamt_raum + (breite * laenge)
 
         print("Noch ein weiteres Teilrechteck?(Y/N)")
-        userinput = input().lower()
+        userinput = yes_no_input()
         if userinput == "n":
             teilrechteck = False
     return gesamt_raum
@@ -50,3 +53,36 @@ def print_list(gesamt_wohnung, raum_durchschnitt, raum_list):
     print(f"Gesamtgröße: {gesamt_wohnung}m²")
     print(f"Durschnittsraumgröße: {raum_durchschnitt}m²")
 
+
+def yes_no_input():
+    pattern = re.compile(r"[yYnN]")
+    not_matching = True
+    while not_matching:
+        eingabe = input()
+        if pattern.match(eingabe):
+            return eingabe.lower()
+        else:
+            print("Falsche Eingabe, bitte erneut eingeben: (Y/N)")
+
+
+def number_input():
+    integer = re.compile(r"\d+")
+    floating = re.compile(r"\d+[.]\d+")
+    not_matching = True
+    while not_matching:
+        zahl = input()
+        if integer.match(zahl) or floating.match(zahl):
+            return zahl
+        else:
+            print("Falsche Eingabe, bitte erneut eingeben: (Ganz- oder Dezimalzahl)")
+
+
+def name_input():
+    pattern = re.compile(r"^[a-zA-ZöäüÄÖÜ]+$")
+    not_matching = True
+    while not_matching:
+        name = input()
+        if pattern.match(name):
+            return name
+        else:
+            print("Falsche Eingabe, bitte erneut eingeben: ")
